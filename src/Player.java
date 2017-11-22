8ebadd9fa1ee02d9a4d7609bbf7401b1361348dd
slatz8075 <slatz8075@HRH-IC0027332.wrdsb.ca>
2017-11-22 10:07:38 -0500

    still trying to figure things out

--- src/Player.java
+++ src/Player.java
@@ -8,13 +8,32 @@
  * @author slatz8075
  */
 public class Player {
-    
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    private int direction = 0;
+    private int location = 0;
 
-        
+    public void move() {
+        //access the location array at the position we are currently at
+
+        //move through the words in this array until we have found the direction we are facing
+        while(location[location]){
+            //move to the next word
+            location[location].next();
+        }
+        //now move through this section until we have found either true or false
+        if ( == true) {
+            //we know that there are no instructions here to follow so do nothing
+        } else {
+            //there
+        }
+    }
+
+    public turnLeft() {
+    }
+
+    public turnRight() {
     }
 }
