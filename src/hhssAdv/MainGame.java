81840c94d86f036e3e12e318fec24352984e9f40
slatz8075 <slatz8075@HRH-IC0027332.wrdsb.ca>
2017-12-05 10:10:43 -0500

    fixed picture problem

--- src/hhssAdv/MainGame.java
+++ src/hhssAdv/MainGame.java
@@ -37,10 +37,10 @@ public class MainGame {
         //try to find the info file
         try {
             //create a url for the location of the info file
-            URL url = MainGame.class.getResource("info.txt");
-            System.out.println(url);
+            URL url = MainGame.class.getResource("src//hhssAdv//info.txt");
+          
             // creating the file reader
-            file = new FileReader(url.getFile());
+            file = new FileReader("src//hhssAdv//info.txt");
         } catch (Exception e) {
             //handle any errors
             e.printStackTrace();
@@ -50,71 +50,68 @@ public class MainGame {
         //use a Scanner with the file
         Scanner in = new Scanner(file);
         //start to fill the location array with locations, and these locations with scenes
+        //but first get the starting information
+        //it is so store this location as the starting location
+        startLoc = in.nextLine();
+        //we should now be at the starting direction information
+        //so store it as such
+        startDir = in.nextLine().charAt(0);
+        //finally move to the start of the real information
         for (int l = 0; in.hasNext(); l++) {
             //create/name a location at this spot
             String name = in.nextLine();
             locations[l] = new Location();
             locations[l].setName(name);
             //using a for loop move through the scene lines
-            for (int s = 0; s < 3; s++) {
+            for (int s = 0; s < 4; s++) {
                 //create an empty scene at this spot
                 Scene scene = new Scene();
                 locations[l].setScene(s, scene);
                 //get this line
                 String sceneLine = in.nextLine();
-                //first check if this is the starting position info at the beginning of the doc
-                if (s == 0) {
-                    //it is so store this location as the starting string
-                    startLoc = sceneLine;
-                } else if (s == 1) {
-                    //this is the starting direction so store it as such
-                    startDir = sceneLine.charAt(0);
-                    //now check to see that this is not a location line (characterized by its second character not being a space
-                } else if (sceneLine.charAt(1) != ' ') {
-                    //it is so break out of the for loop
-                    break;
-                    //new we know that this is a regular scene line
+                //split the sceneLine string by its spaces
+                String[] sceneLineParts = sceneLine.split(" ");
+                //set the specific parts of the scene equal to the array parts
+                scene.setDir(sceneLineParts[0].charAt(0));
+                scene.setPic(sceneLineParts[1]);
+                //check to see if the next part of the sceneLineParts is true
+                if (sceneLineParts[2].substring(0, 4).equals("true")) {
+                    //it is true, so set isFrontBlocked equal to true
+                    locations[l].getScene(s).setIsFrontBlocked(true);
                 } else {
-                    //split the sceneLine string by its spaces
-                    String[] sceneLineParts = sceneLine.split(" ");
-                    //set the specific parts of the scene equal to the array parts
-                    locations[l].getScene(s).setDir(sceneLineParts[0].charAt(0));
-                    locations[l].getScene(s).setPic(sceneLineParts[1]);
-                    //check to see if the next part of the sceneLineParts is true
-                    System.out.println(sceneLineParts[2].substring(0, 4));
-                    //PROBLEM HERE
-                    if (sceneLineParts[2].substring(0, 4).equals("true")) {
-                        //it is true, so set isFrontBlocked equal to true
-                        locations[l].getScene(s).setIsFrontBlocked(true);
-                    } else {
-                        //so we know that it is false, so set it equal to false
-                        locations[l].getScene(s).setIsFrontBlocked(false);
-                        //and now we know that the newDir, and newLoc are existent, set them too
-                        locations[l].getScene(s).setNewLoc(sceneLineParts[3]);
-                        locations[l].getScene(s).setNewDir(sceneLineParts[4].charAt(0));
-                    }
+                    //so we know that it is false, so set it equal to false
+                    locations[l].getScene(s).setIsFrontBlocked(false);
+                    //and now we know that the newDir, and newLoc are existent, set them too
+                    locations[l].getScene(s).setNewLoc(sceneLineParts[3]);
+                    locations[l].getScene(s).setNewDir(sceneLineParts[4].charAt(0));
                 }
             }
-            //find the position in the location array that is equal to the startLoc
-            for (int locPos = 0; locPos < locations.length; locPos++) {
-                //check to see if this location's name is the starting name
-                if (locations[locPos].getName().equals(startLoc)) {
-                    //it is so set the player's inital position equal to this one
-                    pLoc = locPos;
-                    //and break out of the for loop
-                    break;
-                }
+        }
+        System.out.println("done for loop");
+        
+        //find the position in the location array that is equal to the startLoc
+        for (int locPos = 0; locPos < locations.length; locPos++) {
+            System.out.println("location: " + locations[locPos]);
+            //check to see if this location's name is the starting name
+            System.out.println("locPos: " + locPos);
+            //System.out.println("picture: " + locations[locPos].getScene(pSce).getPic());
+
+            if (locations[locPos].getName().equals(startLoc)) {
+                //it is so set the player's inital position equal to this one
+                pLoc = locPos;
+                //and break out of the for loop
+                break;
             }
-            //now set the scene the player is facing equal to its numerical equivalent
-            if (startDir == 'N') {
-                pSce = 0;
-            } else if (startDir == 'E') {
-                pSce = 1;
-            } else if (startDir == 'S') {
-                pSce = 2;
-            } else{
-                pSce = 3;
-            }
+        }
+        //now set the scene the player is facing equal to its numerical equivalent
+        if (startDir == 'N') {
+            pSce = 0;
+        } else if (startDir == 'E') {
+            pSce = 1;
+        } else if (startDir == 'S') {
+            pSce = 2;
+        } else {
+            pSce = 3;
         }
     }
 
@@ -139,5 +136,66 @@ public class MainGame {
     //setting the scene that the player is observing
     public void setSce(int sce) {
         this.pSce = sce;
+    }
+
+    public void turnLeft() {
+        //Check if the direction is equal to North 
+        if (getLoc().getScene(pSce).getDir() == 'N') {
+            //Set the scene to be west
+            pSce = 3;
+        } else if (getLoc().getScene(pSce).getDir() == 'E') {
+            //Set the scene to be north
+            pSce = 0;
+        } else if (getLoc().getScene(pSce).getDir() == 'S') {
+            //Set the scene to be one directyion left of the current direction
+            pSce = 1;
+        } else {
+            //Set the scene to be one directyion left of the current direction
+            pSce = 2;
+        }
+    }
+
+    public void turnRight() {
+        //Check if the direction is equal to North 
+        if (getLoc().getScene(pSce).getDir() == 'N') {
+            //Set the scene to be east
+            pSce = 1;
+        } else if (getLoc().getScene(pSce).getDir() == 'E') {
+            //Set the scene to be south
+            pSce = 2;
+        } else if (getLoc().getScene(pSce).getDir() == 'S') {
+            //Set the scene to be west
+            pSce = 3;
+        } else {
+            //Set the scene to be north
+            pSce = 0;
+        }
+    }
+
+    public void move() {
+        //make sure that we can move
+        if (getLoc().getScene(pSce).getIsFrontBlocked() == false) {
+            //we can so get the new loc and new dir 
+            char pNewDir = getLoc().getScene(pSce).getNewDir();
+            String newLoc = getLoc().getScene(pSce).getNewLoc();
+            //Check if the direction is equal to North 
+            if (pNewDir == 'N') {
+                //Set the players direction to north
+                pSce = 0;
+                //check if its east
+            } else if (pNewDir == 'E') {
+                //Set the players direction to east
+                pSce = 1;
+                //check if its south
+            } else if (pNewDir == 'S') {
+                //Set the players direction to south
+                pSce = 2;
+                //it is not all of these make it west
+            } else {
+                //Set the players direction to west
+                pSce = 3;
+            }
+
+        }
     }
 }
